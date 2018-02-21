c18daa6fe9a547ffe9972f50b230fd501f5ff7fc
Ben Rogers <benjamineffingrogers@gmail.com>
2018-02-21 16:59:24 -0600

    Totally reworked how all of the methods pertaining to player control interact with one another. Added Player class to handle methods that only pertain to the player's units. Will need to figure out if I need to steal any other methods away from Unit class, but so far I'm happy with where every thing is located. Need to do some testing to make sure all of these methods loop and interact correctly, then add status method to give the player stat information of a given unit. Also need to update player turn to only give each unit one move and one attack per turn.

--- ConsoleEmblem.py
+++ ConsoleEmblem.py
@@ -1,10 +1,39 @@
 import numpy
 
 
+class Player:
+    def __init__(self, map_name):
+        self.map_name = map_name
+        self.available_moves = len([o.coords for o in self.map_name.blue_team])
+
+    def choose_unit(self):
+        move_list = [o.coords for o in self.map_name.blue_team]
+        while True:
+            unit_chosen = input('Which unit would you like to pick?')
+            check_int = unit_chosen.split()
+
+            try:
+                check_int = [int(check_int[0]), int(check_int[1])]
+                unit_chosen = check_int
+            except Exception as e:
+                print(e)
+                continue
+            if unit_chosen in move_list:
+                for i in range(len(self.map_name.blue_team)):
+                    if self.map_name.blue_team[i].coords == unit_chosen:
+                        unit_chosen = self.map_name.blue_team[i].player_turn()
+
+            else:
+                print('That is not one of your units.')
+                continue
+            if not unit_chosen:
+                break
 class Board:
 
     squares = []
     units = []
+    blue_team = []
+    red_team = []
 
     def __init__(self, map_size):
         self.size = map_size
@@ -37,16 +66,17 @@ class Board:
                     self.squares[i].symbol = '[]'
                     self.squares[i].filled = False
         for i in range(self.x):
-            print(i + 1, end = '\t')
+            print('{:^4}'.format(i + 1), end='\t')
         for i in range(len(self.squares)):
             if i % self.x == 0:
                 print((i + 1) // 10)
 
-            print(self.squares[i].symbol, end = '\t')
+            # set text centered in a field 4 characters wide
+            print('{:^4}'.format(self.squares[i].symbol), end='\t')
 
         print(self.y)
 
-    @staticmethod
+    @staticmethod #Why does this take x, y coordinates seperately? Fix this garbo
     def get_distance(unit, x, y):
         """This function is used to get the distance, not counting diagonals, between any two spaces on the grid."""
         distance = 0
@@ -68,7 +98,8 @@ class Square:
 
 class Unit:
     valid_moves = []
-    def __init__(self, symbol, coords, board, hp = 1, speed=3, atk_range = 1, atk_power = 1):
+    def __init__(self, symbol, coords, map, hp = 1, speed=3, atk_range = 1, atk_power = 1):
+        self.map = map
         self.symbol = symbol
         self.coords = coords
         self.x = self.coords[0]
@@ -77,111 +108,135 @@ class Unit:
         self.speed = speed
         self.atk_range = atk_range
         self.atk_power = atk_power
-        board.units.append(self)
+        map.units.append(self)
+        if self.symbol.startswith('[|'):
+            self.team = 'Red'
+            map.red_team.append(self)
+        else:
+            self.team = 'Blue'
+            map.blue_team.append(self)
 
 
     def get_coords(self):
         return self.coords
 
-    def player_turn(self, choice):
-        turn_options = ["move", "attack"]
+    def player_turn(self):
+        choice = input('What do you want to do?')
+        turn_options = ["move", "attack", "cancel"]
         while choice not in turn_options:
-            choice = input('What would you like to do?')
+            choice = input('I did not understand. What do you want to do?')
         if choice == "move":
             self.get_valid_spaces()
-            self.move(input("Where do you want to go?").split())
+            self.move()
         elif choice == "attack":
-            self.attack(input("Which square would you like to attack?").split())
+            self.attack()
+        elif choice == "cancel":
+            return 'cancel'
+        elif choice == 'done':
+            return 'done'
+#Fix move, fix attack. Player_turn and player.take_turn on the right track now. Need to changes those names later to make them more distinct
+    def move(self):
 
-    def move(self, coords):
-        valid_input = False
 
-        while not valid_input:
+        while True:
+            coords_chosen = input('To which square would you like to move?')
+            check_int = coords_chosen.split()
             try:
-                destination = [int(coords[0]), int(coords[1])]
-            except:
-                if coords == ['cancel']:
-                    board.move_preview = False
-                    board.display()
-                    self.player_turn(input('What would you like to do?'))
-                    return None
-                else:
-                    coords = input('Incorrect input. Try again!').split()
-                    continue
-            if destination in self.valid_moves:
-                self.x = destination[0]
-                self.y = destination[1]
+                check_int = [int(check_int[0]), int(check_int[1])]
+                coords_chosen = check_int
+            except Exception as e:
+                print(e)
+                if coords_chosen == 'cancel':
+                    self.map.move_preview = False
+                    self.map.display()
+                    return 'cancel'
+                continue
+
+
+            if coords_chosen in self.valid_moves:
+                self.x = coords_chosen[0]
+                self.y = coords_chosen[1]
                 self.coords = [self.x, self.y]
-                for i in range(len(board.squares)):
-                    for j in range(len(board.units)):
-                        if board.squares[i].coords != board.units[j].coords:
-                            board.squares[i].symbol = '[]'
-                            board.squares[i].filled = False
-                        elif board.squares[i].coords == board.units[j].coords:
-                            board.squares[i].symbol = board.units[j].symbol
-                            board.squares[i].filled = True
-                valid_input = True
-            elif destination not in self.valid_moves:
-                return self.move(input("That is not a valid move.").split())
+                for i in range(len(self.map.squares)):
+                    for j in range(len(self.map.units)):
+                        if self.map.squares[i].coords != self.map.units[j].coords:
+                            self.map.squares[i].symbol = '[]'
+                            self.map.squares[i].filled = False
+                        elif self.map.squares[i].coords == self.map.units[j].coords:
+                            self.map.squares[i].symbol = self.map.units[j].symbol
+                            self.map.squares[i].filled = True
+                player.available_moves -= 1
+                del (self.valid_moves[:])
+                return 'done'
+            elif coords_chosen not in self.valid_moves:
+                continue
 
-        del(self.valid_moves[:])
-
-    def attack(self, coords):
+    def attack(self):
         valid_targets = []
-        valid_input = False
-
-        while not valid_input:
+        while True:
+            target_space = input('Which square would you like to attack?')
+            check_int = target_space.split()
             try:
-                target_space = [int(coords[0]), int(coords[1])]
-
-            except:
-                if coords == ['cancel']:
-                    board.move_preview = False
-                    board.display()
-                    self.player_turn(input('What would you like to do?'))
-                    return None
-                else:
-                    coords = input('Incorrect input. Try again!').split()
-                    continue
-
-            for i in range(len(board.units)):
-                if board.get_distance(self, board.units[i].x, board.units[i].y) <= (self.atk_range):
-                    valid_targets.append(board.units[i].coords)
+                check_int = [int(check_int[0]), int(check_int[1])]
+                target_space = check_int
+            except Exception as e:
+                print(e)
+                if target_space == 'cancel':
+                    return 'cancel'
+                continue
+            for i in range(len(self.map.units)):
+                if board.get_distance(self, self.map.units[i].x,self.map.units[i].y) <= self.atk_range:
+                    if self.map.units[i] in self.map.red_team:
+                        valid_targets.append(self.map.units[i].coords)
 
             if target_space in valid_targets:
-                for i in range(len(board.units)):
-                    if board.units[i].coords == target_space:
-                        board.units[i].hp -= self.atk_power
-                        if board.units[i].hp <= 0:
-                            board.units[i].coords = [100, 100]
-                    valid_input = True
-            elif target_space not in valid_targets:
-                return self.attack(input('That is not a valid choice').split())
+                for i in range(len(self.map.units)):
+                    if self.map.units[i].coords == target_space:
+                        self.map.units[i].hp -= self.atk_power
+                        if self.map.units[i].hp <= 0:
+                            self.map.units[i].coords = [100, 100]
+                            for j in range(len(self.map.red_team)):
+                                if self.map.red_team[j] == self.map.units[i]:
+                                    del self.map.red_team[j]
+                return 'done'
 
+            elif target_space not in valid_targets:
+                print('That is not a valid target')
+                continue
     def get_valid_spaces(self):
-        for i in range(len(board.squares)):
-            distance = board.get_distance(self, board.squares[i].x, board.squares[i].y)
-            if distance <= self.speed and board.squares[i].filled == False:
-                board.squares[i].symbol = '[░]'
-                self.valid_moves.append(board.squares[i].coords)
-                board.move_preview = True
+        for i in range(len(self.map.squares)):
+            distance = self.map.get_distance(self, self.map.squares[i].x, self.map.squares[i].y)
+            if distance <= self.speed and self.map.squares[i].filled == False:
+                self.map.squares[i].symbol = '[░]'
+                self.valid_moves.append(self.map.squares[i].coords)
+                self.map.move_preview = True
             elif distance <= (self.speed + self.atk_range):
-                board.squares[i].symbol = '[▒]'
-                board.move_preview = True# Start here. This needs  to be fixed
-        board.display()
+                self.map.squares[i].symbol = '[▒]'
+                self.map.move_preview = True
+        self.map.display()
 
 
 board_size = (10, 8)
 board = Board(board_size)
-player2 = Unit('[b]', [10, 8], board)
-player = Unit('[a]', [10, 7], board)
 
+unit3 = Unit('[c]', [1, 1], board)
+unit2 = Unit('[|b]', [1, 2], board)
+unit = Unit('[a]', [1, 3], board) #Movable character
+
+player = Player(board)
 
 done = 0
-turn_options = ["move", "attack"]
-print(board.units)
-for i in range(len(board.units)):
-    print(board.units[i].coords)
+
+
+
 while not done:
-    board.display()
-    player.player_turn(input('What would you like to do?'))
+    player.available_moves = len(board.blue_team)
+
+    while player.available_moves > 0:
+        board.display()
+        selected_unit = player.choose_unit()
+
+
+
+
+    print('enemy turn goes here')
